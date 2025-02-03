809c95ac86ea5fe10bb2a4a67ccd6b0ccc0a4058
Stefan Mader <st.mader@gmx.de>
2025-02-03 19:58:20 +0100

    !!WIP!! some more work in progress.

--- opengl_refinement.cpp
+++ opengl_refinement.cpp
@@ -21,23 +21,52 @@ GLuint openGLDescriptors[DESCRIPTORIDCOUNT][MAXMIPMAPS] = { {0} };
 GLuint openGLFragmentShader_sampleDescriptors;
 GLuint openGLFragmentShader_refineKeyPoints;
 GLuint openGLVertexShader_basic;
-const std::string sampleDescriptorProgram = "#version 300 es\n"
+GLuint openGLProgram_sampleDescriptors;
+GLuint openGLProgram_refineKeyPoints;
+const std::string sampleDescriptorProgram = "#version 300 es\nprecision highp float;\n"
+"out vec4 frag_color;\n"
+"void main()\n"
+"{\n"
+"    frag_color = vec4(1.0,0.5,0.2,1.0);\n"
+"}\n"
 "\n";
-const std::string refineKeyPointsProgram = "#version 300 es\n"
+const std::string refineKeyPointsProgram = "#version 300 es\nprecision highp float;\n"
+"out vec4 frag_color;\n"
+"void main()\n"
+"{\n"
+"    frag_color = vec4(1.0,0.5,0.2,1.0);\n"
+"}\n"
 "\n";
 const std::string basicProgram = "#version 300 es\n"
+"layout(location = 0) in vec2 pos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(pos.xy, 0.0, 1.0);\n"
+"}\n"
 "\n";
 
 void checkShader(GLuint shader) {
     GLint isCompiled = 0;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
     if (isCompiled == GL_FALSE) {
-        GLint maxLength = 0;
+        GLint maxLength = 0; glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
         std::vector<char> errorLog(maxLength);
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
         glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
         printf("%s", &(errorLog[0]));
         glDeleteShader(shader);
+        exit(0);
+    }
+}
+
+void checkProgram(GLuint program) {
+    GLint isCompiled = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+        GLint maxLength = 0; glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<char> errorLog(maxLength);
+        glGetProgramInfoLog(program, maxLength, &maxLength, &errorLog[0]);
+        printf("%s", &(errorLog[0]));
+        glDeleteProgram(program);
         exit(0);
     }
 }
@@ -58,6 +87,15 @@ GLuint vertexShader(const char* shaderProgram, const unsigned int size) {
     glCompileShader(shader);
     checkShader(shader);
     return shader;
+}
+
+GLuint program(const GLuint vertexShader, const GLuint pixelShader) {
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, pixelShader);
+    glLinkProgram(program);
+    checkProgram(program);
+    return program;
 }
 
 void glfwErrorCallback(int error_code, const char* description) {
@@ -82,6 +120,8 @@ void initOpenGL() {
     openGLFragmentShader_sampleDescriptors = pixelShader(sampleDescriptorProgram.c_str(), sampleDescriptorProgram.length());
     openGLFragmentShader_refineKeyPoints = pixelShader(refineKeyPointsProgram.c_str(), refineKeyPointsProgram.length());
     openGLVertexShader_basic = vertexShader(basicProgram.c_str(), basicProgram.length());
+    openGLProgram_sampleDescriptors = program(openGLVertexShader_basic, openGLFragmentShader_sampleDescriptors);
+    openGLProgram_refineKeyPoints = program(openGLVertexShader_basic, openGLFragmentShader_refineKeyPoints);
 }
 
 void upload2DFloatTexture(const GLuint& tex, const float* data, const unsigned int width, const int height, const bool nearest) {
